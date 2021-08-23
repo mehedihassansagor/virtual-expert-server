87ff6c88306e74ad04ab8ad0fd60a183d1d752d9
borhanshuvo <borhan015@gmail.com>
2021-08-24 01:07:58 +0600

    changes

--- models/AdminLogin.js
+++ models/AdminLogin.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const AdminLoginSchema = new mongoose.Schema(
   {
-    email: {
+    username: {
       type: String,
       required: true,
     },
--- routes/adminLogin.js
+++ routes/adminLogin.js
@@ -1,30 +1,25 @@
 const router = require("express").Router();
 const AdminLogin = require("../models/AdminLogin");
 
-router.get("/", async (req, res) => {
-  try {
-    const adminLogin = await AdminLogin.find({});
-    res.status(200).json(adminLogin);
-  } catch (err) {
-    res.status(404).json(err);
-  }
-});
-
 router.post("/login", async (req, res) => {
   try {
-    const email = req.body.email;
+    const username = req.body.username;
     const password = req.body.password;
     const adminLogin = await AdminLogin.find({
-      email: email,
+      username: username,
     });
     if (adminLogin.length > 0) {
       if (adminLogin[0].password === password) {
-        res.status(200).json(adminLogin[0]);
+        res.status(200).send({ result: adminLogin[0].username });
       } else {
-        res.status(200).json("Authentication ERROR");
+        res.status(200).send({
+          error: "Authentication ERROR",
+        });
       }
     } else {
-      res.status(200).json("Authentication ERROR");
+      res.status(200).send({
+        error: "Authentication ERROR",
+      });
     }
   } catch (err) {
     res.status(404).json(err);
@@ -36,27 +31,6 @@ router.post("/post", async (req, res) => {
     const adminLogin = new AdminLogin(req.body);
     const data = await adminLogin.save();
     res.status(200).json(data);
-  } catch (err) {
-    res.status(404).json(err);
-  }
-});
-
-router.put("/update", async (req, res) => {
-  try {
-    const id = req.body._id;
-    await About.findByIdAndUpdate(
-      { _id: id },
-      {
-        $set: {
-          title: req.body.title,
-          discription: req.body.discription,
-        },
-      },
-      {
-        useFindAndModify: false,
-      }
-    );
-    res.status(200).json("updated");
   } catch (err) {
     res.status(404).json(err);
   }
